712888e34e15d1407128329a21213a6a4d34c65f
nimiyakujjwal <26951554+nimiyakujjwal@users.noreply.github.com>
2024-09-22 20:50:26 +0530

    Alphabet Rangoli question from HackerRank

--- 0-pattern/alpha_rangoli.py
+++ 0-pattern/alpha_rangoli.py
@@ -1,24 +1,33 @@
+""" HackerRank Question: Alpha Rangoli
+The center of the rangoli has the first alphabet letter a, and the boundary has the  alphabet letter (in alphabetical order).
+
+Function Description
+    Complete the rangoli function in the editor below.
+
+rangoli has the following parameters:
+    int size: the size of the rangoli
+
+Returns:
+    string: a single string made up of each of the lines of the rangoli separated by a newline character (\n)
+
+Input Format:
+    Only one line of input containing , the size of the rangoli.
+
+Constraints:
+    0 < size < 27
+
+"""
+
 def print_rangoli(size):
-    # your code goes here
-    
-    # rows = 2 * size - 1
-    
-    # for i in range(1, rows+1):
-    #     if i <= size:
-    #         print(chr(97 + size - i))
-    #     else:
-    #         print(chr(97 + i-size))
-        
-    char = [chr(i) for i in range (97, 97 + size)]
-    width = (2 * size - 1) + ((size - 1) * 2)
-    for i in range (1-size,size):
+    char = [chr(i) for i in range(97, 97 + size)]  # list of lower-case alphabets.
+    width = (2 * size - 1) + ((size - 1) * 2)   # alphabets + no. of '-'
+    for i in range(1-size, size):
         second = list(char[(abs(i)):])
         first = list(reversed(second[1:]))
-        total = first+second
-        print('-'.join(total).center(width,'-'))
+        total = first + second
+        print('-'.join(total).center(width, '-'))
     
     
-
 if __name__ == '__main__':
     n = int(input("Enter the size: "))
     print_rangoli(n)
